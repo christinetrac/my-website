4d8d09287742db343e1c6ccddd10447419d5556c
Christine Trac <43654021+christinetrac@users.noreply.github.com>
2019-03-12 23:24:35 -0400

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,1 +1,9 @@
-js
+$(document).ready(function($) {
+	var Body = $('body');
+	Body.addClass('preloader-site');
+});  
+
+$(window).load(function() {
+	$('.preloader-wrapper').fadeOut();
+	$('body').removeClass('preloader-site');
+});
